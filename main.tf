2a2ab3fd158f74a866b0d5237678631a4a21e5e7
pappik <pappik@gmail.com>
2024-01-04 22:17:32 -0500

    vpc terraform

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
-resource "aws_security_group" "docdb" {
-  name        = "${var.env}-docdb_security_group"
-  description = "${var.env}-docdb_security_group"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    description = "Mongodb"
-    from_port   = 27017
-    to_port     = 27017
-    protocol    = "tcp"
-    cidr_blocks = var.allow_cidr
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = merge(local.common_tags, { Name = "${var.env}-docdb_security_group"} )
-}
-
+#resource "aws_security_group" "docdb" {
+#  name        = "${var.env}-docdb_security_group"
+#  description = "${var.env}-docdb_security_group"
+#  vpc_id      = var.vpc_id
+#
+#  ingress {
+#    description = "Mongodb"
+#    from_port   = 27017
+#    to_port     = 27017
+#    protocol    = "tcp"
+#    cidr_blocks = var.allow_cidr
+#  }
+#
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#
+#  tags = merge(local.common_tags, { Name = "${var.env}-docdb_security_group"} )
+#}
+#
